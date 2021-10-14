8da0faf785133c4bd9e9b30360d05661fe9c5e5c
Thomas Thiebaud <thiebaud.tom@gmail.com>
2021-10-14 19:31:20 +0200

    feat: change the way to get templates

--- packages/cli/lib/creapp-init.js
+++ packages/cli/lib/creapp-init.js
@@ -1,7 +1,34 @@
 const path = require("path");
+const { execSync } = require("child_process");
 const prompts = require("prompts");
 const validatePackageName = require("validate-npm-package-name");
 const fse = require("fs-extra");
+const https = require("https");
+const { PassThrough } = require("stream");
+const gunzip = require("gunzip-maybe");
+const tar = require("tar-fs");
+const mv = require("mv");
+
+async function untarRequest(url, dest) {
+  return new Promise((resolve, reject) => {
+    const stream = new PassThrough();
+    const unzip = gunzip();
+    const untar = tar.extract(dest);
+
+    stream.pipe(unzip).pipe(untar);
+    https.get(url, (response) => {
+      const statusCode = response.statusCode;
+      if (statusCode !== 200) {
+        return reject(new Error("Download error!"));
+      }
+
+      response.pipe(stream);
+
+      stream.on("error", (err) => reject(err));
+      stream.on("finish", () => resolve());
+    });
+  });
+}
 
 async function run() {
   const baseDir = process.cwd();
@@ -28,8 +55,8 @@ async function run() {
         name: "template",
         message: "What template do you want to use?",
         choices: [
-          { title: "react + javascript", value: "react-js" },
-          { title: "react + typescript", value: "react-ts" },
+          { title: "react + javascript", value: "@creapp/template-react-js" },
+          { title: "react + typescript", value: "@creapp/template-react-ts" },
         ],
       },
     ],
@@ -41,12 +68,19 @@ async function run() {
   );
 
   const appDir = path.join(baseDir, response.projectName);
-  const srcDir = path.resolve("./template", response.template);
-
   fse.mkdirSync(appDir);
-  fse.copySync(srcDir, appDir);
+  const url = execSync(`npm view ${response.template} dist.tarball`).toString(
+    "utf-8"
+  );
+  await untarRequest(url, appDir);
+  mv(
+    path.join(appDir, "package"),
+    appDir,
+    { mkdirp: true, clobber: false },
+    () => {}
+  );
 }
 
 run().catch((err) => {
-  console.log(err.message);
+  console.log(err);
 });
