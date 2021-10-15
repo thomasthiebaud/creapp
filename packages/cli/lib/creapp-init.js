15f53e7136539242fe2e5046ce394b3bfc98858b
Thomas Thiebaud <thiebaud.tom@gmail.com>
2021-10-15 10:58:09 +0200

    feat: add more details to @creapp/cli

--- packages/cli/lib/creapp-init.js
+++ packages/cli/lib/creapp-init.js
@@ -1,12 +1,16 @@
 const path = require("path");
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
 const prompts = require("prompts");
 const validatePackageName = require("validate-npm-package-name");
 const fs = require("fs-extra");
+const { promisify } = require("util");
+const { log } = require("@creapp/core");
+const chalk = require("chalk");
+
+const execAsync = promisify(exec);
 
 async function run() {
   const baseDir = process.cwd();
-
   const response = await prompts(
     [
       {
@@ -42,17 +46,24 @@ async function run() {
   );
   const appDir = path.join(baseDir, response.projectName);
   await fs.mkdir(appDir);
-  const tarball = execSync(`npm pack ${response.template} --silent`, {
+  log("Downloading template...", false);
+  const res = await execAsync(`npm pack ${response.template} --silent`, {
     cwd: appDir,
-  })
-    .toString("utf-8")
-    .trim();
-  execSync(`tar -xf ${tarball} --strip-components=1`, { cwd: appDir }).toString(
-    "utf-8"
-  );
+  });
+  const tarball = res.stdout.trim();
+  log(" done");
+  log("Extracting template...", false);
+  await execAsync(`tar -xf ${tarball} --strip-components=1`, { cwd: appDir });
   await fs.remove(path.join(appDir, tarball));
+  log(" done");
+  log(chalk`{bold Almost there!}`);
+  log(
+    chalk`Go to your project {dim cd ${path.relative(process.cwd(), appDir)}}`
+  );
+  log(chalk`Install dependencies {dim npm install}`);
+  log(chalk`Start the app {dim npm start}`);
 }
 
 run().catch((err) => {
-  console.log(err);
+  log(err);
 });
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,11 +1,20 @@
 const path = require("path");
 const fs = require("fs");
-
+const { EOL } = require("os");
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+function log(msg, newLine = true) {
+  process.stdout.write(msg);
+
+  if (newLine) {
+    process.stdout.write(EOL);
+  }
+}
 
 module.exports = {
   appPath: resolveApp("."),
   appHtml: resolveApp("index.html"),
   appTsConfig: resolveApp("tsconfig.json"),
+  log,
 };
