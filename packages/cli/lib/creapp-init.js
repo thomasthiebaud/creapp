54be35c4416b262e57febe72669479789b7da2e0
Thomas Thiebaud <thiebaud.tom@gmail.com>
2021-10-15 08:22:09 +0200

    fix: extract tarball correctly

--- packages/cli/lib/creapp-init.js
+++ packages/cli/lib/creapp-init.js
@@ -2,33 +2,7 @@ const path = require("path");
 const { execSync } = require("child_process");
 const prompts = require("prompts");
 const validatePackageName = require("validate-npm-package-name");
-const fse = require("fs-extra");
-const https = require("https");
-const { PassThrough } = require("stream");
-const gunzip = require("gunzip-maybe");
-const tar = require("tar-fs");
-const mv = require("mv");
-
-async function untarRequest(url, dest) {
-  return new Promise((resolve, reject) => {
-    const stream = new PassThrough();
-    const unzip = gunzip();
-    const untar = tar.extract(dest);
-
-    stream.pipe(unzip).pipe(untar);
-    https.get(url, (response) => {
-      const statusCode = response.statusCode;
-      if (statusCode !== 200) {
-        return reject(new Error("Download error!"));
-      }
-
-      response.pipe(stream);
-
-      stream.on("error", (err) => reject(err));
-      stream.on("finish", () => resolve());
-    });
-  });
-}
+const fs = require("fs-extra");
 
 async function run() {
   const baseDir = process.cwd();
@@ -43,7 +17,7 @@ async function run() {
           if (!validatePackageName(name).validForNewPackages) {
             return `${name} is not a valid project name`;
           }
-          if (fse.existsSync(path.join(baseDir, name))) {
+          if (fs.existsSync(path.join(baseDir, name))) {
             return `A directory "${name}" already exists in "${baseDir}"`;
           }
 
@@ -66,19 +40,17 @@ async function run() {
       },
     }
   );
-
   const appDir = path.join(baseDir, response.projectName);
-  fse.mkdirSync(appDir);
-  const url = execSync(`npm view ${response.template} dist.tarball`).toString(
+  await fs.mkdir(appDir);
+  const tarball = execSync(`npm pack ${response.template} --silent`, {
+    cwd: appDir,
+  })
+    .toString("utf-8")
+    .trim();
+  execSync(`tar -xf ${tarball} --strip-components=1`, { cwd: appDir }).toString(
     "utf-8"
   );
-  await untarRequest(url, appDir);
-  mv(
-    path.join(appDir, "package"),
-    appDir,
-    { mkdirp: true, clobber: false },
-    () => {}
-  );
+  await fs.remove(path.join(appDir, tarball));
 }
 
 run().catch((err) => {
